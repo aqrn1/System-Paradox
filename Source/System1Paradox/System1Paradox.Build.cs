c81e18c4013195e14d8e219c8d060c840dce0604
aqrn1 <gagawka44@mail.ru>
2025-11-26 20:43:07 +0300

    Auto-sync: 26.11.2025 20:43:07,04

--- Source/System1Paradox/System1Paradox.Build.cs
+++ Source/System1Paradox/System1Paradox.Build.cs
@@ -4,41 +4,24 @@ using UnrealBuildTool;
 
 public class System1Paradox : ModuleRules
 {
-	public System1Paradox(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public System1Paradox(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] {
-			"Core",
-			"CoreUObject",
-			"Engine",
-			"InputCore",
-			"EnhancedInput",
-			"AIModule",
-			"StateTreeModule",
-			"GameplayStateTreeModule",
-			"UMG",
-			"Slate"
-		});
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput"
+            // Убрали лишние зависимости пока не понадобятся
+        });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { });
+        // Убрали PrivateDependencyModuleNames - пока не нужны
+        // Убрали PublicIncludePaths - ссылались на удалённые папки
 
-		PublicIncludePaths.AddRange(new string[] {
-			"System1Paradox",
-			"System1Paradox/Variant_Horror",
-			"System1Paradox/Variant_Horror/UI",
-			"System1Paradox/Variant_Shooter",
-			"System1Paradox/Variant_Shooter/AI",
-			"System1Paradox/Variant_Shooter/UI",
-			"System1Paradox/Variant_Shooter/Weapons"
-		});
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Раскомментируйте по мере необходимости:
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+    }
 }
